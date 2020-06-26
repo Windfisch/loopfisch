46b121caa269f52d9f0b2ba2954ee73192ddc247
Florian Jung <flo@windfis.ch>
2020-06-26 03:01:34 +0200

    license

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+// loopfisch -- A loop machine written in rust.
+// Copyright (C) 2020 Florian Jung <flo@windfis.ch>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License verion 3 as
+// published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 use jack;
 use std::cmp::{min,max};
 
