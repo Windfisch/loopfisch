536baa6e40af42d0db361b9c135a9c8f1ca18151
Florian Jung <flo@windfis.ch>
2020-11-16 02:31:38 +0100

    allow POSTing miditakes

--- src/rest_api/post.rs
+++ src/rest_api/post.rs
@@ -6,9 +6,17 @@ use serde::Deserialize;
 use super::updates::*;
 use super::util::gen_unique_name;
 
+
+#[derive(Deserialize,Clone,PartialEq)]
+pub enum TakeType {
+	Audio,
+	Midi
+}
+
 #[derive(Deserialize,Clone)]
 pub struct TakePost {
 	name: Option<String>,
+	r#type: TakeType,
 }
 
 #[derive(Deserialize,Clone)]
@@ -80,43 +88,55 @@ pub async fn post_take(state: State<'_, std::sync::Arc<GuiState>>, synthid: u32,
 	let guard = &mut *guard_;
 	if let Some(synth) = guard.synths.iter_mut().find(|s| s.id == synthid) {
 		if let Some(chain) = synth.chains.iter_mut().find(|c| c.id == chainid) {
-			let audio_id = guard.take_id.gen();
-			let midi_id = guard.take_id.gen();
-			let name = gen_unique_name(data.name.as_deref().unwrap_or("Take"), chain.takes.iter().map(|c|&c.name[..]));
-
-			let mut associated_midi_takes: Vec<u32> =
-				chain.takes.iter()
-					.filter( |t| t.is_midi() && t.is_audible() )
-					.map(|t| t.id)
-					.collect();
-			associated_midi_takes.push(midi_id);
-
 			// FIXME this is racy! there should be an atomic function for adding multiple takes at once!
 			// FIXME and the unwrap... there is so much wrong with this.
-			let engine_miditake_id = guard.engine.add_miditake(synth.engine_mididevice_id, true).unwrap();
-			let engine_audiotake_id = guard.engine.add_audiotake(chain.engine_audiodevice_id, false).unwrap();
+			let name = gen_unique_name(data.name.as_deref().unwrap_or("Take"), chain.takes.iter().map(|c|&c.name[..]));
 
-			chain.takes.push( Take {
-				id: audio_id,
-				engine_take_id: EngineTakeRef::Audio(engine_audiotake_id),
-				name: name.clone(),
-				muted: true,
-				muted_scheduled: false,
-				state: RecordingState::Waiting,
-				associated_midi_takes
-			});
-			state.update_list.push(make_update_take(chain.takes.last().unwrap(), synthid, chainid)).await;
+			// set up the MIDI take
+			let midi_id = guard.take_id.gen();
+			let engine_miditake_id = guard.engine.add_miditake(synth.engine_mididevice_id, true).unwrap();
+
 			chain.takes.push( Take {
 				id: midi_id,
 				engine_take_id: EngineTakeRef::Midi(engine_miditake_id),
-				name,
+				name: name.clone(),
 				muted: false,
 				muted_scheduled: false,
 				state: RecordingState::Waiting,
 				associated_midi_takes: Vec::new()
 			});
 			state.update_list.push(make_update_take(chain.takes.last().unwrap(), synthid, chainid)).await;
-			return Ok(rocket::response::status::Created::new(format!("/api/synths/{}/chains/{}/takes/{}", synthid, chainid, audio_id)));
+
+			let result_take_id;
+			// set up the audio take, if requested.
+			if data.r#type == TakeType::Audio {
+				let audio_id = guard.take_id.gen();
+				let engine_audiotake_id = guard.engine.add_audiotake(chain.engine_audiodevice_id, false).unwrap();
+
+				let mut associated_midi_takes: Vec<u32> =
+					chain.takes.iter()
+						.filter( |t| t.is_midi() && t.is_audible() )
+						.map(|t| t.id)
+						.collect();
+				associated_midi_takes.push(midi_id);
+
+				chain.takes.push( Take {
+					id: audio_id,
+					engine_take_id: EngineTakeRef::Audio(engine_audiotake_id),
+					name,
+					muted: true,
+					muted_scheduled: false,
+					state: RecordingState::Waiting,
+					associated_midi_takes
+				});
+				state.update_list.push(make_update_take(chain.takes.last().unwrap(), synthid, chainid)).await;
+				result_take_id = audio_id;
+			}
+			else {
+				result_take_id = midi_id;
+			}
+
+			return Ok(rocket::response::status::Created::new(format!("/api/synths/{}/chains/{}/takes/{}", synthid, chainid, result_take_id)));
 		}
 	}
 	Err(Status::NotFound)
